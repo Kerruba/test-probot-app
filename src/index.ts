5f2dbc41639bb84acbfdee8df1e3cd8fbfc04980
Luca Cherubin <luca.cherubin@gmail.com>
2020-10-31 17:16:28 +0100

    update readme

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+  app.on('push', async (context) => {
+      if (context.payload.installation === undefined) {
+        console.log("installation id is undefined")
+        return
+      }
+
+      console.log("installation id is ", context.payload.installation)
+      const installationId = context.payload.installation.id;
+      
+      const github = app.auth(installationId)
+
+      console.log(github)
+      
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
